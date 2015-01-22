9a5ac780fbd117bd6ebfaa452f78dd63bf0083dd
Arun Endapally <arun.e.kumar@gmail.com>
2015-01-23 03:21:10 +0530

    SSO

---
+++ SSO/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SSO.Controllers
+{
+  public class AccountController : Controller
+  {
+    [AllowAnonymous]
+    public ActionResult Login(string returnUrl)
+    {
+      if (Request.IsAuthenticated)
+      {
+        return RedirectToAction("Index", "Home");
+      }
+
+      ViewBag.ReturnUrl = returnUrl;
+      return View();
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Login(string username, string password, string returnUrl)
+    {
+      if (FormsAuthentication.Authenticate(username, password))
+      {
+        FormsAuthentication.SetAuthCookie(username, false);
+        if (!string.IsNullOrEmpty(returnUrl))
+        {
+          return Redirect(returnUrl);
+        }
+        else
+        {
+          return RedirectToAction("Index", "Home");
+        }
+      }
+      else
+      {
+        ModelState.AddModelError(string.Empty, "Invalid login details");
+        ViewBag.ReturnUrl = returnUrl;
+        return View();
+      }
+    }
+  }
+}
